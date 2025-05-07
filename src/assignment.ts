cebc67d32c74fda1169664ad29d495e1807a8943
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-07 22:41:48 +0600

    Problem 2 done

--- src/assignment.ts
+++ src/assignment.ts
@@ -1,3 +1,17 @@
-function formatString(input: string, toUpper: boolean=true):string{
-    return toUpper ? input.toUpperCase(): input.toLocaleLowerCase()
+function formatString(input: string, toUpper: boolean = true): string {
+  return toUpper ? input.toUpperCase() : input.toLocaleLowerCase();
 }
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+console.log(filterByRating(books));
