0b9259f61a3f86bbce3a20dd8ecc2c035692b2dd
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-08 16:15:12 +0600

    Problem 6 done

--- src/assignment.ts
+++ src/assignment.ts
@@ -50,3 +50,21 @@ function processValue(value: string | number): number {
     return value * 2;
   }
 }
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products?.length === 0) {
+    return null;
+  }
+  let mostExpensive = products[0];
+  for (const product of products) {
+    if (product.price > mostExpensive.price) {
+      mostExpensive = product;
+    }
+  }
+  return mostExpensive;
+}
