891a32a5893f3252c7f9ae38015453ebd7ef263c
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-08 16:21:56 +0600

    Problem 7 done

--- src/assignment.ts
+++ src/assignment.ts
@@ -68,3 +68,22 @@ function getMostExpensiveProduct(products: Product[]): Product | null {
   }
   return mostExpensive;
 }
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function getDayType(day: Day): string {
+  if (day === Day.Friday) {
+    return "Weekend";
+  } else {
+    return "Weekday";
+  }
+}
+console.log(getDayType(Day.Friday));
