7f2a8188d86847262c7f4235d8bc1735141671d4
Abdul Wahed Nur <wahednur@gmail.com>
2025-05-08 13:54:19 +0600

    Proble 4 done

--- src/assignment.ts
+++ src/assignment.ts
@@ -18,4 +18,31 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arrays.reduce((acc, curr) => acc.concat(curr), []);
 }
 
-console.log(concatenateArrays([1, 2], [3, 4], [5]));
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  public getInfo(): string {
+    return ` Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(model: string, year: number, make: string) {
+    super(make, year);
+    this.model = model;
+  }
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+console.log(myCar.getInfo());
+console.log(myCar.getModel());
